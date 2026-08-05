faa830d114e4ae0c039c226f9df009ae373ed352
josecamilo1457 <josecamilobarletta@outlook.com>
2026-08-05 15:08:55 -0300

    Store GCLID from URL in localStorage
    Add functionality to store GCLID from URL in localStorage.

--- js/tracking.js
+++ js/tracking.js
@@ -1,1 +1,11 @@
+(() => {
+  // Obtener el GCLID de la URL
+  const params = new URLSearchParams(window.location.search);
+  const gclid = params.get("gclid");
 
+  // Si existe, guardarlo en el navegador
+  if (gclid) {
+    localStorage.setItem("gclid", gclid);
+    console.log("GCLID guardado:", gclid);
+  }
+})();
